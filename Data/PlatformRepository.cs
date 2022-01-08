32a8f959c8e1c5475cbeb93c72f74bb64e59490f
Aleksandr Bespalov <bespalovav@pik.ru>
2022-01-08 16:36:25 +0300

    Data layer:
    - Add Platform Model.
    - Add AppDbContext.
    - Add PlatformRepository.
    - Add PreparationDb.
    - Add AutoMapper.

---
+++ Data/PlatformRepository.cs
@@ -0,0 +1,33 @@
+﻿using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        private readonly AppDbContext context;
+
+        public PlatformRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            context.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAll() => context.Platforms;
+
+        public Platform GetPlatformById(int id) => context.Platforms.FirstOrDefault(t => t.Id == id);
+
+        public bool SaveChanges() => context.SaveChanges() >= 0;
+    }
+}
---
+++ Data/PreparationDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public static class PreparationDb
+    {
+        public static void PreparePopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (context.Platforms.Any())
+            {
+                Console.WriteLine("--> We already have data.");
+            } else
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Service Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}

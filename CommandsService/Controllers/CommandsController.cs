5ded16de1d0a64e7c254d937b7f0ec22b12ad663
Aleksandr Bespalov <bespalovav@pik.ru>
2022-01-26 23:56:55 +0300

    Add CommandsController to the Commands Service.

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepository repository;
+        private readonly IMapper mapper;
+
+        public CommandsController(ICommandRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit {nameof(GetCommandsForPlatform)}: {platformId}.");
+
+            if (!repository.PlatformExits(platformId))
+            {
+                return NotFound();
+            }
+
+            var commandItems = repository.GetCommandsForPlatform(platformId);
+            var commandDtos = mapper.Map<IEnumerable<CommandReadDto>>(commandItems);
+            return Ok(commandDtos);
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Hit {nameof(GetCommandForPlatform)}: {platformId} / {commandId}.");
+
+            if (!repository.PlatformExits(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = repository.GetCommand(platformId, commandId);
+            if (command == null)
+            {
+                return NotFound();
+            }
+
+            var commandDto = mapper.Map<CommandReadDto>(command);
+            return Ok(commandDto);
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto commandDto)
+        {
+            Console.WriteLine($"--> Hit {nameof(CreateCommandForPlatform)}: {platformId} / {JsonSerializer.Serialize(commandDto)}.");
+
+            if (!repository.PlatformExits(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = mapper.Map<Command>(commandDto);
+
+            repository.CreateCommand(platformId, command);
+            repository.SaveChanges();
+
+            var commandReadDto = mapper.Map<CommandReadDto>(command);
+
+            return CreatedAtRoute(nameof(GetCommandForPlatform), new { platformId = platformId, commandId = commandReadDto.Id }, commandReadDto);
+        }
+    }
+}
--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +13,25 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
+        private readonly ICommandRepository repository;
+        private readonly IMapper mapper;
        
-        public PlatformsController()
+        public PlatformsController(ICommandRepository repository, IMapper mapper)
         {
-
+            this.repository = repository;
+            this.mapper = mapper;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService...");
+
+            var platformItems = repository.GetAllPlatforms();
+            var platformDtos = mapper.Map<IEnumerable<PlatformReadDto>>(platformItems);
+            return Ok(platformDtos);
+        }
+
 
         [HttpPost]
         public ActionResult TestInboundConnection()
